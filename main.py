d8a2180d241a73e96a692699876af46d55e62840
bushuev-alex <bushuev-alex@mail.ru>
2022-12-30 11:57:50 +0700

    1. rename tictactoe.py to main.py 2. rename game.py to tictactoe.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from tictactoe import TicTacToe
+
+
+my_game = TicTacToe()
+my_game.greet()
+my_game.gamer = "X"
+my_game.draw_table()
+
+while True:
+    print(f"\n'{my_game.gamer}' moves")
+    coordinates = my_game.get_new_coordinates()
+    my_game.make_move(coordinates, my_game.gamer)
+    my_game.draw_table()
+    rows, cols, diag_lr, diag_rl = my_game.get_table_thrimers()
+    result = my_game.get_result(rows, cols, diag_lr, diag_rl)
+    if result != 'Game not finished':
+        print(result)
+        break
+    my_game.change_gamer()
--- tictactoe.py
+++ tictactoe.py
@@ -1,19 +1,75 @@
-from game import TicTacToe
+import numpy as np
+from dataclasses import dataclass
 
 
-my_game = TicTacToe()
-my_game.greet()
-my_game.gamer = "X"
-my_game.draw_table()
+@dataclass(slots=True, frozen=True)
+class Coordinates:
+    x: int
+    y: int
 
-while True:
-    print(f"\n'{my_game.gamer}' moves")
-    coordinates = my_game.get_new_coordinates()
-    my_game.make_move(coordinates, my_game.gamer)
-    my_game.draw_table()
-    rows, cols, diag_1, diag_2 = my_game.get_table_thrimers()
-    result = my_game.get_result(rows, cols, diag_1, diag_2)
-    if result != 'Game not finished':
-        print(result)
-        break
-    my_game.change_gamer()
+
+class TicTacToe:
+
+    def __init__(self):
+        self.lines = [[' ' for _ in range(3)] for _ in range(3)]
+        self.gamer = None
+
+    def greet(self):
+        print("-----------------")
+        print("  Greeting you   ")
+        print("     in game     ")
+        print("   Tic-Tac-Toe   ")
+        print("-----------------")
+        print("input format: x y")
+        print("  x - row number ")
+        print("  y - col number ")
+        print("-----------------\n")
+
+    def draw_table(self) -> None:
+        print(' ', ' ', *[1, 2, 3])
+        print('  ---------')
+        for n, line in enumerate(self.lines):
+            print(n + 1, '|', *line, '|')
+        print('  ---------')
+
+    def change_gamer(self) -> None:
+        self.gamer = "X" if self.gamer == "O" else "O"
+
+    def make_move(self, coordinates: Coordinates, gamer: str) -> None:
+        self.lines[coordinates.x - 1][coordinates.y - 1] = gamer
+
+    def get_new_coordinates(self) -> Coordinates:
+        while True:
+            try:
+                x, y = [int(i) for i in input("Enter the coordinates: ").split()]
+
+                if not (1 <= x <= 3 and 1 <= y <= 3):
+                    print("Coordinates should be from 1 to 3!")
+                    continue
+                if not (self.lines[x - 1][y - 1] == ' '):
+                    print('This cell is occupied! Choose another one!')
+                    continue
+                return Coordinates(x, y)
+            except ValueError:
+                print("You should enter 2 numbers!")
+
+    def get_table_thrimers(self) -> tuple:
+        matrix = np.asarray(self.lines)
+        rows = [''.join(matrix[:, i]) for i in range(3)]
+        cols = [''.join(matrix[i, :]) for i in range(3)]
+        diag_lr = [''.join(matrix.diagonal())]
+        diag_rl = [''.join(np.fliplr(matrix).diagonal())]
+        return rows, cols, diag_lr, diag_rl
+
+    def get_result(self, rows: list, cols: list, diag_lr: list, diag_rl: list) -> str:
+        thrimers = rows + cols + diag_lr + diag_rl
+        if any(map(lambda x: x == "XXX", thrimers)):  # "XXX" in row/col, returns X wins
+            return 'X wins'
+        elif any(map(lambda x: x == "OOO", thrimers)):  # "OOO" in row/col, returns O wins
+            return 'O wins'
+        elif all(map(lambda x: " " not in x, rows)):  # there isn't " " (and nobody wins) in all table, returns Draw
+            return 'Draw'
+        elif any(map(lambda x: " " in x, rows)):  # if any " " in all table, returns Game not finished
+            return 'Game not finished'
+        else:
+            return 'Impossible'
