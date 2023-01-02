ebbb5eb989e14abfa14c17d79deac0de531950fa
bushuev-alex <bushuev-alex@mail.ru>
2023-01-02 12:57:50 +0700

    1. add AI-mode playing with 2 levels of difficulty (easy, medium) 2. add one-more dataclass: class FieldLines

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ from tictactoe import TicTacToe
 
 
 my_game = TicTacToe()
-my_game.greet()
-my_game.gamer = "X"
-my_game.draw_table()
+command, levels = my_game.get_command_and_gamers()
 
-while True:
-    print(f"\n'{my_game.gamer}' moves")
-    coordinates = my_game.get_new_coordinates()
-    my_game.make_move(coordinates, my_game.gamer)
+if command == "start":
+    my_game.set_levels_to_gamers(levels)
+    my_game.greet()
+    my_game.gamer = "X"
     my_game.draw_table()
-    rows, cols, diag_lr, diag_rl = my_game.get_field_lines()
-    result = my_game.get_game_result(rows, cols, diag_lr, diag_rl)
-    if result != 'Game not finished':
-        print(result)
-        break
-    my_game.change_gamer()
+
+    while True:
+        print(f"\n'{my_game.gamer}' moves")
+        coordinates = my_game.get_new_coordinates(my_game.gamer)
+        my_game.make_move(coordinates, my_game.gamer)
+        my_game.draw_table()
+        field_lines = my_game.get_field_lines(my_game.field)
+        result = my_game.get_game_result(field_lines)
+        if result != 'Game not finished':
+            print(result)
+            break
+        my_game.change_gamer()
--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 from dataclasses import dataclass
 
@@ -8,11 +10,21 @@ class Coordinates:
     y: int
 
 
+@dataclass(slots=True, frozen=True)
+class FieldLines:
+    rows: list
+    cols: list
+    diag_lr: list
+    diag_rl: list
+
+
 class TicTacToe:
 
     def __init__(self):
         self.field = [[' ' for _ in range(3)] for _ in range(3)]
         self.gamer = None
+        self.gamers = {"X": None, "O": None}
+        self.user_list = ("user", "easy", "medium", "hard")
 
     def greet(self):
         print("\n-----------------")
@@ -24,6 +36,26 @@ class TicTacToe:
         print("  x - row number ")
         print("  y - col number ")
         print("-----------------\n")
+
+    def get_command_and_gamers(self) -> tuple:
+        while True:
+            command, *levels = input("Input start/exit and command (user/easy/medium/hard): ").split(' ')
+            if command == "exit":
+                return command, levels
+            if command not in ("start", "exit"):
+                print("Bad parameters! Enter right command: 'start' or 'exit'.")
+                continue
+            if len(levels) != 2:
+                print("Should be 2 gamers!")
+                continue
+            if any([level not in self.user_list for level in levels]):
+                print("Bad parameters! Wrong gamer name.")
+                continue
+            return command, levels
+
+    def set_levels_to_gamers(self, levels):
+        self.gamers["X"] = levels[0]
+        self.gamers["O"] = levels[1]
 
     def draw_table(self) -> None:
         print(' ', ' ', '1', '2', '3')
@@ -38,10 +70,39 @@ class TicTacToe:
     def make_move(self, coordinates: Coordinates, gamer: str) -> None:
         self.field[coordinates.x - 1][coordinates.y - 1] = gamer
 
-    def get_new_coordinates(self) -> Coordinates:
+    def check_if_win_next_step(self, x: int, y: int, gamer: str) -> bool:
+        field = self.field.copy()
+        if field[x][y] == ' ':
+            field[x][y] = gamer
+            field_lines = self.get_field_lines(field)
+            result = self.get_game_result(field_lines)
+            field[x][y] = ' '
+            if any([result == 'X wins', result == 'O wins', result == 'Draw']):
+                return True
+
+    def get_coordinates(self, gamer) -> tuple:
+        if self.gamers[gamer] == "user":
+            x, y = [int(i) for i in input("Enter the coordinates: ").split()]
+            return x, y
+        if self.gamers[gamer] == "easy":
+            x, y = random.choice([1, 2, 3]), random.choice([1, 2, 3])
+            return x, y
+        if self.gamers[gamer] == "medium":
+            for x in range(3):
+                for y in range(3):
+                    if self.check_if_win_next_step(x, y, gamer):
+                        return x + 1, y + 1
+                    if self.check_if_win_next_step(x, y, list({"X", "O"}.difference(gamer))[0]):
+                        return x + 1, y + 1
+            x, y = random.choice([1, 2, 3]), random.choice([1, 2, 3])
+            return x, y
+        if self.gamers[gamer] == "hard":
+            pass
+
+    def get_new_coordinates(self, gamer: str) -> Coordinates:
         while True:
             try:
-                x, y = [int(i) for i in input("Enter the coordinates: ").split()]
+                x, y = self.get_coordinates(gamer)
 
                 if not (1 <= x <= 3 and 1 <= y <= 3):
                     print("Coordinates should be from 1 to 3!")
@@ -53,23 +114,23 @@ class TicTacToe:
             except ValueError:
                 print("You should enter 2 numbers!")
 
-    def get_field_lines(self) -> tuple:
-        matrix = np.asarray(self.field)
-        rows = [''.join(matrix[:, i]) for i in range(3)]
-        cols = [''.join(matrix[i, :]) for i in range(3)]
+    def get_field_lines(self, field: list) -> FieldLines:
+        matrix = np.asarray(field)
+        rows = [''.join(matrix[i, :]) for i in range(3)]
+        cols = [''.join(matrix[:, i]) for i in range(3)]
         diag_lr = [''.join(matrix.diagonal())]
         diag_rl = [''.join(np.fliplr(matrix).diagonal())]
-        return rows, cols, diag_lr, diag_rl
+        return FieldLines(rows, cols, diag_lr, diag_rl)
 
-    def get_game_result(self, rows: list, cols: list, diag_lr: list, diag_rl: list) -> str:
-        lines = rows + cols + diag_lr + diag_rl
-        if any(map(lambda x: x == "XXX", lines)):  # "XXX" in row/col, returns X wins
+    def get_game_result(self, field_lines: FieldLines) -> str:
+        all_lines = field_lines.rows + field_lines.cols + field_lines.diag_lr + field_lines.diag_rl
+        if any(map(lambda x: x == "XXX", all_lines)):  # "XXX" in row/col, returns X wins
             return 'X wins'
-        elif any(map(lambda x: x == "OOO", lines)):  # "OOO" in row/col, returns O wins
+        elif any(map(lambda x: x == "OOO", all_lines)):  # "OOO" in row/col, returns O wins
             return 'O wins'
-        elif all(map(lambda x: " " not in x, rows)):  # there isn't " " (and nobody wins) in all table, returns Draw
+        elif all(map(lambda x: " " not in x, field_lines.rows)):  # there isn't " " (and nobody wins) in all table, returns Draw
             return 'Draw'
-        elif any(map(lambda x: " " in x, rows)):  # if any " " in all table, returns Game not finished
+        elif any(map(lambda x: " " in x, field_lines.rows)):  # if any " " in all table, returns Game not finished
             return 'Game not finished'
         else:
             return 'Impossible'
